f5f3e565b3384d92ca6ef91ff001cd40bcad3a4d
Luan0Avila <avila.almeida.luan@gmail.com>
2025-02-24 21:36:05 -0300

    criando um super user

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
 # Register your models here.
+class Category_admin(admin.ModelAdmin):
+    ...
+
+
+class Recipe_Admin( admin.ModelAdmin):
+    ...
+
+@admin.register(Recipe)
+class Recipe_Admin( admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, Category_admin)
