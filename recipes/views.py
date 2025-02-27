3fa161c840f27dd704e8a06ac85f0c2873f5dd4c
Luan0Avila <avila.almeida.luan@gmail.com>
2025-02-27 20:51:19 -0300

    ocultando receitas não publicadas/ corrigindo category e autor nulo

--- recipes/views.py
+++ recipes/views.py
@@ -6,14 +6,17 @@ from .models import Recipe
 
 #HTTP request
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+    is_published= True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={ 'recipes': recipes,})
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id
+        category__id=category_id,
+        is_published=True,
         ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={ 'recipes': recipes,})
+    return render(request, 'recipes/pages/category.html', context={ 'recipes': recipes,})
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
