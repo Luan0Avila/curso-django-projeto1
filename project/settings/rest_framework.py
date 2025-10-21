e9705e9b86f29ed4a68ab8a013e61d916b88fb01
Luan0AVila <avila.almeida.luan@gmail.com>
2025-10-21 12:59:31 -0300

    finzalizando as configs do simple jwt

--- project/settings/rest_framework.py
+++ project/settings/rest_framework.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+import os
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
@@ -6,3 +9,14 @@ REST_FRAMEWORK = {
     )
 }
 
+# Django project settings.py
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "BLACKLIST_AFTER_ROTATION": False,
+    "SIGNING_KEY": os.environ.get('SECRET_KEY', 'INSECURE'),
+    "AUTH_HEADER_TYPES": ("Bearer",),
+
+}
