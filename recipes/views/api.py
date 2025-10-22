dca92a0ed3b416194235d95a8f351bd7fa987356
Luan0Avila <avila.almeida.luan@gmail.com>
2025-10-22 20:29:49 -0300

    usando permission

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,31 +1,45 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from tag.models import Tag
-from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
 from ..models import Recipe
 from ..serializers import RecipeSerializer, TagSerializer
-from rest_framework.pagination import PageNumberPagination 
-from rest_framework.viewsets import ModelViewSet
+
 
 class RecipeAPIv2Pagination(PageNumberPagination):
-    page_size = 10
+    page_size = 2
+
 
 class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
+    permission_classes = [IsAuthenticated, ]
+
+    def get_serializer_class(self):
+        return super().get_serializer_class()
+
+    def get_serializer(self, *args, **kwargs):
+        return super().get_serializer(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["example"] = 'this is in context now'
+        return context
 
     def get_queryset(self):
-        qs =super().get_queryset()
-        category_id = self.request.query_params.get('category_id', None)
+        qs = super().get_queryset()
+
+        category_id = self.request.query_params.get('category_id', '')
 
         if category_id != '' and category_id.isnumeric():
             qs = qs.filter(category_id=category_id)
+
         return qs
-    
 
     def partial_update(self, request, *args, **kwargs):
         pk = kwargs.get('pk')
@@ -43,6 +57,7 @@ class RecipeAPIv2ViewSet(ModelViewSet):
         return Response(
             serializer.data,
         )
+
 
 @api_view()
 def tag_api_detail(request, pk):
