c2e652410ce6a7f69dce78eb86eba877fca5c56d
Luan0Avila <avila.almeida.luan@gmail.com>
2025-10-26 16:53:16 -0300

    preparando a base de dados para o deploy

--- project/settings/databases.py
+++ project/settings/databases.py
@@ -1,10 +1,15 @@
 from .environment import BASE_DIR
+import os
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DATABASE_ENGINE'),
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABES_HOST'),
+        'PORT': os.environ.get('DATABSE_PORT'),
     }
 }
